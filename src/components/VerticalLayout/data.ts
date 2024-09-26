1ca81d8217b338eed87700fc265f624482b7b3e4
AyoMoses1 <ayomoses111@gmail.com>
2024-09-27 00:07:02 +0100

    resolve sidebar configuration

---
+++ src/components/VerticalLayout/data.ts
@@ -0,0 +1,52 @@
+export const sidebarLinks = [
+  {
+    path: "/dashboard",
+    icon: "bx bx-home-circle",
+    label: "Dashboard",
+  },
+  {
+    path: "/apps-filemanager",
+    icon: "bx bx-file",
+    label: "File Manager",
+  },
+  {
+    path: "/calendar",
+    icon: "bx bx-calendar",
+    label: "Calendar",
+  },
+  {
+    path: "/chat",
+    icon: "bx bx-chat",
+    label: "Conversations",
+  },
+  {
+    path: "/ecommerce-customers",
+    icon: "bx bx-file",
+    label: "Contacts",
+  },
+  {
+    path: "/email-inbox",
+    icon: "bx bx-file",
+    label: "Emails",
+  },
+  {
+    path: "/invoices-list",
+    icon: "bx bx-file",
+    label: "Invoices",
+  },
+  {
+    path: "/projects-grid",
+    icon: "bx bx-file",
+    label: "Projects",
+  },
+  {
+    path: "/tasks-kanban",
+    icon: "bx bx-file",
+    label: "Deals",
+  },
+  {
+    path: "/contacts-list",
+    icon: "bx bx-file",
+    label: "Users",
+  },
+];
